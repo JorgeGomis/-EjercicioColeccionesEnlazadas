a14d12327be0d0a7bbd8d828190d33c1a447cac4
PorrasGabriel <93255769+PorrasGabriel@users.noreply.github.com>
2023-04-23 17:46:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,22 +7,29 @@ internal class Program
         ListaDoblementeEnlazada<int> ld = new ListaDoblementeEnlazada<int>();
         ld.AñadeAlPrincipio(4);
         ld.AñadeAlPrincipio(3);
-        Console.WriteLine(ld);
+        ld.ImprimeLista();
+
         ld.Clear();
         ld.AñadeAlFinal(6);
         ld.AñadeAlFinal(9);
         ld.AñadeAlPrincipio(3);
-        Console.WriteLine(ld);
+        ld.ImprimeLista();
+        
         NodoListaDoblementeEnlazada<int> nodo = ld.Busca(6);
         ld.AñadeAntesDe(nodo, 5);
         ld.AñadeAntesDe(ld.Primero, 1);
         ld.AñadeDespuesDe(nodo, 7);
         ld.AñadeDespuesDe(ld.Ultimo, 12);
-        Console.WriteLine(ld);
-        // ld.Borra(nodo);
-        // ld.Borra(ld.Primero);
-        // ld.Borra(ld.Ultimo);
-        // Console.WriteLine(ld);
+        ld.ImprimeLista();
 
+        ld.Borra(nodo);
+        ld.Borra(ld.Primero);
+        ld.Borra(ld.Ultimo);
+        ld.ImprimeLista();
+
+        ld.Clear();
+        ld.ImprimeLista();
+        
+        Console.ReadLine();
     }
 }
